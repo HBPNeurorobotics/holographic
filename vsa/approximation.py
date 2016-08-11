80198d9a078769abcffe02aa0e99b41a9387709b
Daniel Spies <spies@fzi.de>
2016-08-11 19:30:48 +0200

    Adapt approximation code to updated HRR library

--- vsa/approximation.py
+++ vsa/approximation.py
@@ -15,7 +15,7 @@ class Approximation:
         else:
             HRR.set_size(HRR.size)
 
-    def learn(self, in_range=(-1.0, 1.0), n_samples=200, fn=None, stddev=0.03):
+    def learn(self, in_range=(-1.0, 1.0), n_samples=200, fn=None, stddev=0.03, use_incremental=True):
         if fn is not None:
             self.fn = fn
             HRR.reset_kernel()
@@ -25,30 +25,57 @@ class Approximation:
         #print("HRR size: {}, input_range: {}, stddev {}".format(HRR.size, HRR.input_range, HRR.stddev))
         A = np.linspace(float(in_range[0]), float(in_range[1]), n_samples)
         #print("A: {}".format(A))
-        samples = np.empty((n_samples, HRR.size), dtype=float)
-        # sample function and store one HRR per sample
-        for i, A_i in enumerate(A):
-            B_i = self.fn(A_i)  # evaluate ith sample
-            samples[i] = (HRR(B_i) % A_i).memory  # probe HRR
+        if use_incremental:
+            # initialize T
+            B_0 = self.fn(A[0])
+            self.T = HRR(B_0) % A[0]
+            # sample function and sequentially update the approximation vector
+            for A_i in A[1:]:
+                B_i = self.fn(A_i)  # evaluate ith sample
+                self.T = self.T ** (HRR(B_i) % A_i)  # update T
+        else:
+            samples = np.empty((n_samples, HRR.size), dtype=float)
+            for i, A_i in enumerate(A):
+                B_i = self.fn(A_i)  # evaluate ith sample
+                samples[i] = (HRR(B_i) % A_i).memory  # probe HRR
+            self.T = HRR(0, generator=samples)
 
-        #print("samples: {}".format(samples))
-        self.T = HRR(0, generator=samples)
-        #print("learn: {}".format(self.T))
+        #self.T.plot()
+        #print("learn: {}".format(self.T.memory))
 
     def plot_result(self, n_samples=10):
         X = np.linspace(HRR.input_range[0], HRR.input_range[1], n_samples)
         Y_hrr = np.empty(n_samples, dtype=float)
+        Y_hrr2 = np.empty(n_samples, dtype=float)
+        Y_hrrsupp = np.empty(n_samples, dtype=float)
         Y_np = np.empty(n_samples, dtype=float)
         for i, x in enumerate(X):
             A = HRR(x)
             B = A * self.T
             #HRR.plot(HRR.reverse_permute(HRR(x).memory))
             #HRR.plot(HRR.reverse_permute((A * self.T).memory))
-            Y_hrr[i] = B.decode()
+            temp = B.decode(return_dict=True)
+            if len(temp) > 1:
+                Y_hrr[i] = temp.keys()[1]
+            else:
+                Y_hrr[i] = temp.keys()[0]
+            Y_hrr2[i] = temp.keys()[0]
+            if len(temp) > 1:
+                # TODO: this is a hack, find something more intelligent
+                temp = B.decode(return_dict=False, suppress_value=x)
+                Y_hrrsupp[i] = temp
+            else:
+                Y_hrrsupp[i] = Y_hrr2[i]
+            #Y_hrr[i] = B.decode(return_dict=True).values()[1]
+            #Y_hrr[i] = temp
             Y_np[i] = self.fn(x)
             #print("HRR: f({}) = {} / truth: {} error: {}".format(x, Y_hrr[i], Y_np[i], Y_np[i] - Y_hrr[i]))
         plt.figure()
-        plt.plot(X, Y_np, 'g')
-        plt.plot(X, Y_hrr, 'rx-')
+        h_np, = plt.plot(X, Y_np, 'g', label="Ground truth")
+        h_hrr, = plt.plot(X, Y_hrr, 'cx--', label="2nd peak if avail")
+        h_hrr2, = plt.plot(X, Y_hrr2, 'bx--', label="1st peak")
+        h_suppr, = plt.plot(X, Y_hrrsupp, 'rx-', label="Suppressed input x")
+        plt_handles = [h_np, h_hrr, h_hrr2, h_suppr]
+        plt.legend(handles=plt_handles, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
         plt.show()
 
