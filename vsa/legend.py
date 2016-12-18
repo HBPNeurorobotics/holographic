7756e4ea36fd35c55f3418f19cdda8d6cf45c068
Alex Lesi <lesi@fzi.de>
2016-12-18 21:40:55 +0100

    New images, few corrections

--- vsa/legend.py
+++ vsa/legend.py
@@ -5,7 +5,7 @@ def getLegend(v,start):
     sh = len(v)
     linestyles = ['-', '--', '-.', ':']
     for i in range(sh):
-        plt.plot(v[i], linestyles[3-i*4/sh], label=start*pow(2,i))
+        plt.plot(v[i], linestyles[3-i*4/sh], label=start*pow(4,i))
     plt.axis([0,len(v[0]),0,1.01])
     plt.xlabel('Bindings')
     plt.ylabel('Accuracy')
