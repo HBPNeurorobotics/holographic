fcf867e9219913403a82f32bb5c79bee94bc31ca
Alex Lesi <lesi@fzi.de>
2016-12-18 18:03:51 +0100

    More Corections - semi-final

--- vsa/legend.py
+++ vsa/legend.py
@@ -7,5 +7,7 @@ def getLegend(v,start):
     for i in range(sh):
         plt.plot(v[i], linestyles[3-i*4/sh], label=start*pow(2,i))
     plt.axis([0,len(v[0]),0,1.01])
+    plt.xlabel('Bindings')
+    plt.ylabel('Accuracy')
     legend = plt.legend(loc='lower left', shadow=True, fontsize='x-large')
     plt.show()
